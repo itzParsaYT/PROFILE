0859255ca62fa8204b097ecb7ef732ace18a48fa
itzParsaYT <itzparsayc2@gmail.com>
2025-02-12 16:06:57 +0330

    Update lsb_custom.sh

--- airootfs/usr/bin/lsb_custom.sh
+++ airootfs/usr/bin/lsb_custom.sh
@@ -2,3 +2,4 @@ rm -rf /etc/lsb-release
 mv /usr/bin/lsb_custom_conf /etc/lsb-release
 pip install pydub thefuzz speechbrain transformers ffmpeg-python sv_ttk darkdetect --break-system-packages 
 pip install git+https://github.com/openai/whisper.git git+https://github.com/mmabrouk/chatgpt-wrapper.git --break-system-packages
+mv lib/py_libs lib/python3.13
